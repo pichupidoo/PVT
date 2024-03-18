1856067882d11b79094a90ce00a7a862fc599ae3
Andrew <gosharak12345@mail.ru>
2024-03-18 14:23:25 +0700

    Add

---
+++ lab2/main.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <time.h>
+#include <inttypes.h>
+#include <omp.h>
+#include <stdlib.h>
+
+double wtime ( )
+{
+    clock_t start, end;
+    double cpu_time_used;
+  double value;
+
+  value = ( double ) clock ( ) 
+        / ( double ) CLOCKS_PER_SEC;
+
+  return value;
+}
+
+void matrix_vector_product_omp(double *a, double *b, double *c, int m, int n)
+{
+#pragma omp parallel
+    {
+        int nthreads = omp_get_num_threads();
+        int threadid = omp_get_thread_num();
+        int items_per_thread = m / nthreads;
+        int lb = threadid * items_per_thread;
+        int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
+        for (int i = lb; i <= ub; i++)
+        {
+            c[i] = 0.0;
+            for (int j = 0; j < n; j++)
+                c[i] += a[i * n + j] * b[j];
+        }
+    }
+}
+
+void run_parallel(int m, int n)
+{
+    double *a, *b, *c;
+    // Allocate memory for 2-d array a[m, n]
+    a = (double *)malloc(sizeof(*a) * m * n);
+    b = (double *)malloc(sizeof(*b) * n);
+    c = (double *)malloc(sizeof(*c) * m);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            a[i * n + j] = i + j;
+    }
+    for (int j = 0; j < n; j++)
+        b[j] = j;
+    double t = wtime();
+    matrix_vector_product_omp(a, b, c, m, n);
+    t = wtime() - t;
+    printf("Elapsed time (parallel): %.6f sec.\n", t);
+    free(a);
+    free(b);
+    free(c);
+}
+
+void matrix_vector_product(double *a, double *b, double *c, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        c[i] = 0.0;
+        for (int j = 0; j < n; j++)
+            c[i] += a[i * n + j] * b[j];
+    }
+}
+
+void run_serial(int m, int n)
+{
+    double *a, *b, *c;
+    
+    a = (double *)malloc(sizeof(*a) * m * n);
+    b = (double *)malloc(sizeof(*b) * n);
+    c = (double *)malloc(sizeof(*c) * m);
+    
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            a[i * n + j] = i + j;
+    }
+    for (int j = 0; j < n; j++)
+        b[j] = j;
+    double t = wtime();
+    matrix_vector_product(a, b, c, m, n);
+    t = wtime() - t;
+    printf("Elapsed time (serial): %.6f sec.\n", t);
+    free(a);
+    free(b);
+    free(c);
+}
+
+int main(int argc, char **argv)
+{
+    int m = 25000, n = 25000;
+    printf("Matrix-vector product (c[m] = a[m, n] * b[n]; m = %d, n = %d)\n", m, n);
+    printf("Memory used: %" PRIu64 " MiB\n", ((m * n + m + n) * sizeof(double)) >> 20);
+    run_serial(m ,n);
+    run_parallel(m, n);
+    return 0;
+}
